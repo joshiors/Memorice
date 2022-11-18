2c2acde84461afff961757f2f5a9a7739e74545c
Matías <61324528+joshiors@users.noreply.github.com>
2022-11-18 19:04:47 -0300

    versión 2.5
    Versión que repara los errores de la 2.0. Versión estable.

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const {readdir, rename} = require("fs");
 
 function imagenes(){
 
-    readdir('img/input', (err, files) => {
-        for(let i=0; i<files.length; i++){
-            rename(`img/input/${files[i]}`, `img/output/i${i}.png`, err => {
-                if (err) throw err;
-                console.log('Renombramiento completado!');
-              });
-        }
+    readdir('src/img/output', (err, filesout) => {
+        const cantOutput = filesout.length <= 1 ? 0 : filesout.length-1;
+
+        readdir('src/img/input', (err, files) => {
+
+            for(let i=0; i<files.length; i++){
+                let i1=cantOutput;
+
+                if(files[i].includes('.md') === false){
+                    rename(`src/img/input/${files[i]}`, `src/img/output/i${i+i1}.png`, err => {
+                        if (err) throw err;
+                        console.log('Renombramiento completado!');
+                    });
+                }
+            }
+    
+        });
     });
 }
 
